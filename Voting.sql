c06d348e6c0142d2b3b4555beb4df0ade8f7cd18
bhu20 <bhumit.n@somaiya.edu>
2024-10-16 08:31:48 +0530

    Add SQL file

---
+++ Voting.sql
@@ -0,0 +1,30 @@
+Create Database Voting;
+Use Voting;
+Select * FROM student;
+select * from Candidates;
+select * from admin;
+insert into admin
+values("7759792e-d9d8-49d4-9d25-908c3836c4c2","$2a$12$uF3AFmAfWbesT1/AikdtF.NRzA4fSazYeMZrC7hH6xqTDYPvN2leG");
+
+-- manage candidates script
+/*
+    document.addEventListener("DOMContentLoaded", function() {
+        fetch('/api/admin/checkLoginStatus')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                console.log(response.json);
+                return response.json();
+            })
+            .then(data => {
+                if (!data.loggedIn) {
+                    window.location.href = "adminlogin/adminlogin.html";
+                }
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+                window.location.href = "adminlogin/adminlogin.html";
+            });
+    });
+    */
